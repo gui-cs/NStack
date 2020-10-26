773f46507d28ae895be906641825d1de50a6ef4f
BDisp <bd.bdisp@gmail.com>
2020-10-26 22:31:18 +0000

    Fixes #47. ColumnWidth needs to differentiate between  non-printable and null characters.

--- NStack/unicode/Rune.ColumnWidth.cs
+++ NStack/unicode/Rune.ColumnWidth.cs
@@ -82,17 +82,15 @@ namespace System
 	        /// <summary>
 	        /// Number of column positions of a wide-character code.   This is used to measure runes as displayed by text-based terminals.
 	        /// </summary>
-	        /// <returns>The width in columns, 0 if the argument is the null character, -1 if the value is not printable, otherwise the number of columsn that the rune occupies.</returns>
+	        /// <returns>The width in columns, 0 if the argument is the null character, -1 if the value is not printable, otherwise the number of columns that the rune occupies.</returns>
 	        /// <param name="r">The red component.</param>
-	        public static int ColumnWidth (Rune rune) 
+	        public static int ColumnWidth (Rune rune)
 	        {
 			uint irune = (uint)rune;
-			if (irune < 32)
-				return 0;
+			if (irune < 32 || (irune >= 0x7f && irune <= 0xa0))
+				return -1;
 			if (irune < 127)
 				return 1;
-			if (irune >= 0x7f && irune <= 0xa0)
-				return 0;
 			/* binary search in table of non-spacing characters */
 			if (bisearch (irune, combining, combining.GetLength (0)-1) != 0)
 				return 0;
--- NStackTests/RuneTest.cs
+++ NStackTests/RuneTest.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 namespace NStackTests {
 	public class RuneTest {
-		public RuneTest ()
+		Rune a = 'a';
+		Rune b = 'b';
+		Rune c = 123;
+		Rune d = '\u1150';  // 0x1150	ᅐ	Unicode Technical Report #11
+		Rune e = '\u1161';  // 0x1161	ᅡ	null character with column equal to 0
+		Rune f = 31;    // non printable character
+		Rune g = 127;   // non printable character
+
+		[Test]
+		public void TestColumnWidth()
 		{
-			Rune a = 'a';
-			Rune b = 'b';
+			var rt = new RuneTest();
+
+			Assert.AreEqual(1, Rune.ColumnWidth(rt.a));
+			Assert.AreEqual(1, Rune.ColumnWidth(rt.b));
 			var l = a < b;
-			Rune c = 123;
+			Assert.IsTrue(l);
+			Assert.AreEqual(1, Rune.ColumnWidth(rt.c));
+			Assert.AreEqual(2, Rune.ColumnWidth(rt.d));
+			Assert.AreEqual(0, Rune.ColumnWidth(rt.e));
+			Assert.AreEqual(-1, Rune.ColumnWidth(rt.f));
+			Assert.AreEqual(-1, Rune.ColumnWidth(rt.g));
 		}
 	}
 }
