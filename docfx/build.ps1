6b6366f8caa522a41da00ae5f797e14f382394c3
Tig Kindel <tig@users.noreply.github.com>
2022-09-17 10:38:05 -0700

    Merged offline changes (#72)
    * Create dotnet.yml
    
    Upgrading CI/CD to GithubActions
    
    * .NET 6
    
    * Updating workflows
    
    * github package
    
    * tweaking yaml
    
    * Fixed path?
    
    * removed path
    
    * added path back
    
    * ugh
    
    * added gitver
    
    * tweaking ver
    
    * new giver stuff for 18.3
    
    * new publish workflow
    
    * added nuget.org publishing
    
    * renamed master to main
    
    * Cleaned up old build files
    
    * Disable publishing to GitHub Packages
    
    * Disable publishing to GitHub Packages
    
    * Revert to v 0.17
    
    * reverted readme
    
    * Moved to Gui-cs Org
    
    * Creating develop branch & setting up gitflow model
    
    * Auto gen API docs
    
    * Nuke old docs
    
    * Update build script for gitflow
    
    * fixes doc gen error
    
    * Fixing api doc generation, again
    
    * Fixed api link
    
    * Removed old refs to migueldeicaza and tig

--- docfx/build.ps1
+++ docfx/build.ps1
@@ -3,7 +3,10 @@
 dotnet clean --configuration Release ../NStack.sln
 dotnet build --configuration Release ../NStack.sln
 
-rm ../docs -Recurse -Force
+rm ../docs -Recurse -Force -ErrorAction SilentlyContinue
+
+
+$env:DOCFX_SOURCE_BRANCH_NAME="main"
 
 
 $env:DOCFX_SOURCE_BRANCH_NAME="main"
