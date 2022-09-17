ec7f9784ec153d45048ce876007a8648fd4f3c11
Charlie Kindel <tig@users.noreply.github.com>
2022-09-17 10:04:35 -0600

    fixes doc gen error

--- docfx/build.ps1
+++ docfx/build.ps1
@@ -3,7 +3,7 @@
 dotnet clean --configuration Release ../NStack.sln
 dotnet build --configuration Release ../NStack.sln
 
-rm ../docs -Recurse -Force
+rm ../docs -Recurse -Force -ErrorAction SilentlyContinue
 
 
 $env:DOCFX_SOURCE_BRANCH_NAME="main"
